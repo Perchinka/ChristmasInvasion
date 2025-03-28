d3f28a941ea18eed188af0ab71485046d972b3e1
Perchinka <alexsator.lukin@gmail.com>
2025-03-28 00:43:04 +0000

    Fix piegun logic

--- Entities/Weapon/Projectiles/BowlingBall/BowlingBall.gd
+++ Entities/Weapon/Projectiles/BowlingBall/BowlingBall.gd
@@ -3,27 +3,40 @@ class_name BowlingBall
 
 
 func _ready() -> void:
+	add_to_group("bowling_ball")
 	super()
-	lifetime = INF
 	gravity_scale = 1.0
 	velocity = velocity.normalized() * speed
+	lifetime = INF
 
 
 func _physics_process(delta: float) -> void:
+	velocity.y += gravity_scale * delta
 	var collision = move_and_collide(velocity * delta)
 	if collision:
 		_handle_collision(collision)
 
 
 func _handle_collision(collision: KinematicCollision2D) -> void:
-	if (
-		collision.get_collider().is_in_group("floor")
-		and collision.get_normal().dot(Vector2(0, -1)) > 0.9
-	):
+	var collider = collision.get_collider()
+	var normal = collision.get_normal()
+
+	if collider.is_in_group("player"):
 		return
 
-	velocity = velocity.bounce(collision.get_normal()).normalized() * speed
-	if impact_effect:
-		var effect_instance = impact_effect.instantiate()
-		effect_instance.global_position = collision.position
-		get_tree().current_scene.add_child(effect_instance)
+	if collider.is_in_group("ground") and normal.dot(Vector2(0, -1)) > 0.9:
+		velocity = velocity.slide(normal)
+		return
+
+	if (
+		(collider.is_in_group("ground") and abs(normal.dot(Vector2(1, 0))) > 0.9)
+		or collider.is_in_group("bowling_ball")
+	):
+		velocity = velocity.bounce(normal).normalized() * speed
+		if impact_effect:
+			var effect_instance = impact_effect.instantiate()
+			effect_instance.global_position = collision.position
+			get_tree().current_scene.add_child(effect_instance)
+		return
+
+	velocity = velocity.slide(normal)
